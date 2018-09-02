300d06da33d6465adcbfe55fb571135d24251938
kanios <mikolajkania44@gmail.com>
2018-09-02 19:49:33 +0200

    Initial commit with skeleton of application

---
+++ src/main/java/com/unidash/unidash/UnidashApplication.java
@@ -0,0 +1,12 @@
+package com.unidash.unidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidashApplication.class, args);
+	}
+}
