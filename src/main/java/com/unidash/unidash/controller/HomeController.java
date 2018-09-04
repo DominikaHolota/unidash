cb90e0924bc6c3dabd8608ece909492c7c981b9a
kanios <mikolajkania44@gmail.com>
2018-09-04 18:16:08 +0200

    thymeleaf + bootstrap

--- src/main/java/com/unidash/unidash/controller/HomeController.java
+++ src/main/java/com/unidash/unidash/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.unidash.unidash.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "ELO";
+        return "index";
     }
 
 }
