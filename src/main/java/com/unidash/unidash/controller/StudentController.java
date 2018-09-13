a45e5290d212cb709808d9ce82f83a51adff4f26
Dominika Holota <atthis@o2.pl>
2018-09-13 22:55:41 +0200

    Dodanie live reload dla folderu static, dodanie testowego controllera

--- src/main/java/com/unidash/unidash/controller/StudentController.java
+++ src/main/java/com/unidash/unidash/controller/StudentController.java
@@ -1,10 +1,10 @@
 package com.unidash.unidash.controller;
 
-import com.unidash.unidash.dao.StudentDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+        import com.unidash.unidash.dao.StudentDao;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PathVariable;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
