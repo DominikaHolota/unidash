5a25bdd0986dff95ae14aac3a2adc1bbdef3bc0a
Dominika Holota <atthis@o2.pl>
2018-10-09 00:13:59 +0200

    usunięcie data.sql - robiła bajzel w userach

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `unidash`.`users` (`email`, `is_active`, `lastname`, `name`, `password`, `role`)
-VALUES ('user@user.pl', 1, 'last', 'imie', 'haslo', 'student'),
-  ('student@student.pl', 1, 'student', 'piotr', 'haslostudent', 'student'),
-  ('admin@admin.pl', 1, 'admin', 'zenek', 'hasloadmin', 'admin'),
-  ('profe@profe.pl', 1, 'uczyciel', 'mariusz', 'hasloteacher', 'teacher');
