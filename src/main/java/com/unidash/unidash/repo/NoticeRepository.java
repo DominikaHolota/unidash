988c676881257a7bb62fe2ab2ded17e7d1129c1d
Dominika Holota <atthis@o2.pl>
2018-10-14 22:49:12 +0200

    dodanie listy ogloszeń

--- src/main/java/com/unidash/unidash/repo/NoticeRepository.java
+++ src/main/java/com/unidash/unidash/repo/NoticeRepository.java
@@ -3,9 +3,15 @@ package com.unidash.unidash.repo;
 import com.unidash.unidash.entity.Notices;
 import com.unidash.unidash.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notices, Integer> {
     Notices findById(int id);
+
+    @Query(value = "SELECT * FROM notices ORDER BY date DESC LIMIT 5;", nativeQuery = true)
+    List<Notices> findLastFive();
 }
